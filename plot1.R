003058bae0ebe99c00c647168947277802e5b2b5
SwarnenduSengupta <sonaisg@rediffmail.com>
2016-05-29 17:51:04 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,6 +5,6 @@ dataC$date<-as.Date(dataC$Date,format="%d/%m/%Y")
 subdata<-subset(dataC,subset=(Date>="2007-02-01" & Date<="2007-02-02"))
 datetime <- paste(as.Date(subdata$Date), subdata$Time)
 subdata$Datetime <- as.POSIXct(datetime)
-
+hist(subdata$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
